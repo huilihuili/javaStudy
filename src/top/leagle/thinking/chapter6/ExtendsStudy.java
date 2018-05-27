f068a23d2d2baac1442bc95671851d63a9b9e592
huilihuili <864814732@qq.com>
2018-05-27 09:43:48 +0800

    thinking in java

---
+++ src/top/leagle/thinking/chapter6/ExtendsStudy.java
@@ -0,0 +1,16 @@
+package top.leagle.thinking.chapter6;
+
+class Game {
+	public Game(int i) {
+		System.out.println("Game constructor");
+	}
+}
+
+class BoardGame extends Game {
+	public BoardGame(int i) {
+		super(i);
+	}
+}
+
+public class ExtendsStudy {
+}
