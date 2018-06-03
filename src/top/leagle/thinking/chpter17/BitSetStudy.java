1ce57cf3d5af7c4b3a9bc99f35aacde5ce0893dd
huilihuili <864814732@qq.com>
2018-06-03 17:48:57 +0800

    Thinking in Java

---
+++ src/top/leagle/thinking/chpter17/BitSetStudy.java
@@ -0,0 +1,11 @@
+package top.leagle.thinking.chpter17;
+
+import org.junit.Test;
+
+public class BitSetStudy {
+
+	@Test
+	public void test1() {
+
+	}
+}
---
+++ src/top/leagle/thinking/chpter17/HashCodeStudy.java
@@ -0,0 +1,5 @@
+package top.leagle.thinking.chpter17;
+
+public class HashCodeStudy {
+
+}
