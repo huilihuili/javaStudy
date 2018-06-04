54c2ef847f4656b2189aa75bfe15eedf8eb87ba4
huilihuili <864814732@qq.com>
2018-06-04 16:16:56 +0800

    Thinking in Java

--- src/top/leagle/thinking/chpter17/BitSetStudy.java
+++ src/top/leagle/thinking/chpter17/BitSetStudy.java
@@ -1,11 +1,22 @@
 package top.leagle.thinking.chpter17;
 
+import java.util.BitSet;
+
 import org.junit.Test;
 
 public class BitSetStudy {
 
 	@Test
 	public void test1() {
+		BitSet bitSet = new BitSet();
+		bitSet.set(0);
+		bitSet.set(1);
+		System.out.println(bitSet);
+		System.out.println(bitSet.size());
 
+		for (int i = 0; i < bitSet.size(); i++) {
+			System.out.print(bitSet.get(i) ? "1" : "0");
+		}
+		System.out.println();
 	}
 }
--- src/top/leagle/thinking/chpter17/HashCodeStudy.java
+++ src/top/leagle/thinking/chpter17/HashCodeStudy.java
@@ -1,5 +1,9 @@
 package top.leagle.thinking.chpter17;
 
+import org.junit.Test;
+
 public class HashCodeStudy {
 
+	@Test
+	public void test1() {}
 }
