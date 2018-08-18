9ca5a37dba099244b975d94d91e2efe5de552aa7
huilihuili <864814732@qq.com>
2018-08-18 16:07:13 +0800

    OverrideTest List

---
+++ src/top/leagle/basic/OverrideTest.java
@@ -0,0 +1,28 @@
+package top.leagle.basic;
+
+import java.util.List;
+
+public class OverrideTest {
+
+	public static void main(String[] args) {
+		// OverrideTest.method(null);
+	}
+
+	// public static void method(List<String> list) {
+	// System.out.println("String list");
+	// }
+	//
+	// public static void method(List<Integer> list) {
+	// System.out.println("Integer list");
+	// }
+
+	// public static String method(List<String> list) {
+	// System.out.println("String list");
+	// return null;
+	// }
+	//
+	// public static Integer method(List<Integer> list) {
+	// System.out.println("Integer list");
+	// return null;
+	// }
+}
